2c37a0f73dbde10320e0026da83b72aac4ceb9e6
Shawn Patel <appdev.shawn@gmail.com>
2020-03-21 23:16:55 -0700

    get running/walking distance

--- UCSF Health/Utilities/Health.swift
+++ UCSF Health/Utilities/Health.swift
@@ -19,7 +19,48 @@ class Health {
     }
     
     public func getData(completion: @escaping(Result<HealthData, Error>) -> Void) {
+        var steps: [Step]!
+        var distances: [Distance]!
+        var flights: [Flight]!
         
+        let myGroup = DispatchGroup()
+        
+        myGroup.enter()
+        getSteps() { response in
+            switch response {
+            case .success(let stepsResult):
+                steps = stepsResult
+                myGroup.leave()
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+        
+        myGroup.enter()
+        getDistances() { response in
+            switch response {
+            case .success(let distancesResult):
+                distances = distancesResult
+                myGroup.leave()
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+        
+        myGroup.enter()
+        getFlights() { response in
+            switch response {
+            case .success(let flightsResult):
+                flights = flightsResult
+                myGroup.leave()
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+        
+        myGroup.notify(queue: .main) {
+            completion(.success(HealthData(steps, distances, flights)))
+        }
     }
     
     private func getSteps(completion: @escaping(Result<[Step], Error>) -> Void) {
@@ -45,10 +86,10 @@ class Health {
             
             var steps: [Step] = []
             stepSamples.enumerateStatistics(from: startDate, to: today) { statistics, error in
-                if let dailySteps = statistics.sumQuantity() {
+                if let dailyStep = statistics.sumQuantity() {
                     let date = statistics.startDate
-                    let stepCount = dailySteps.doubleValue(for: .count())
-                    let step = Step(date, stepCount)
+                    let count = dailyStep.doubleValue(for: .count())
+                    let step = Step(date, count)
                     
                     steps.append(step)
                 }
@@ -60,7 +101,45 @@ class Health {
         healthStore.execute(stepsQuery)
     }
     
-    private func getDistance(completion: @escaping(Result<[Date: Double], Error>) -> Void) {
+    private func getDistances(completion: @escaping(Result<[Distance], Error>) -> Void) {
+        let today = Date()
+        let startDate = Calendar.current.date(byAdding: .day, value: -30, to: today)!
+        
+        var interval = DateComponents()
+        interval.day = 1
+        
+        var anchorComponents = Calendar.current.dateComponents([.day, .month, .year], from: today)
+        anchorComponents.hour = 0
+        
+        let anchorDate = Calendar.current.date(from: anchorComponents)!
+        
+        guard let distances = HKSampleType.quantityType(forIdentifier: .distanceWalkingRunning) else { fatalError() }
+        let distancesQuery = HKStatisticsCollectionQuery(quantityType: distances, quantitySamplePredicate: nil, options: [.cumulativeSum], anchorDate: anchorDate, intervalComponents: interval)
+        
+        distancesQuery.initialResultsHandler = { query, results, error in
+            guard let distanceSample = results else {
+                completion(.failure(error!))
+                return
+            }
+            
+            var distances: [Distance] = []
+            distanceSample.enumerateStatistics(from: startDate, to: today) { statistics, error in
+                if let dailyDistance = statistics.sumQuantity() {
+                    let date = statistics.startDate
+                    let length = dailyDistance.doubleValue(for: .mile())
+                    let distance = Distance(date, length)
+                    
+                    distances.append(distance)
+                }
+            }
+            
+            completion(.success(distances))
+        }
+        
+        healthStore.execute(distancesQuery)
+    }
+    
+    private func getFlights(completion: @escaping(Result<[Flight], Error>) -> Void) {
         
     }
     
